9d88a6adac735cd00867d7e5ec94178627a63244
jpesg <42676743+jpesg@users.noreply.github.com>
2019-10-21 14:56:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -13,41 +13,7 @@ app.get('/', (req, res) => {
 });
 
 // Adds support for GET requests to our webhook
-app.get('/webhook', function(req, res){
 
-    // Your verify token. Should be a random string.
-    let VERIFY_TOKEN = 'token';
-
-    // Parse the query params
-    let mode = req.query['hub.mode'];
-    let token = req.query['hub.verify_token'];
-    let challenge = req.query['hub.challenge'];
-
-    // Checks if a token and mode is in the query string of the request
-    if (mode && token) {
-
-        // Checks the mode and token sent is correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            // Responds with the challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-
-        } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-        }
-    }
-});
-
-// Creates the endpoint for our webhook
-app.post('/webhook', function(req, res){
-
-    let body = req.body;
-
-
-  res.status(200).send('EVENT_RECEIVED');
-});
 
 app.listen(5000, () => {
     console.log('listening on port 5000');
