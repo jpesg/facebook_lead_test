a222af6e071a80db2356dbf706edb79096473222
jpesg <42676743+jpesg@users.noreply.github.com>
2019-10-21 14:58:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var cors = require('cors');
+
 
 var app = express();
 
 app.use(bodyParser.json());
 
-app.use(cors());
+
 
 app.get('/', (req, res) => {
     res.send('health');
