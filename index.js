959a78aaa8bfe5270b45f910c3f03ea1be484a31
jpesg <42676743+jpesg@users.noreply.github.com>
2019-10-21 13:49:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -28,32 +28,34 @@ app.get(['/facebook', '/instagram'], function(req, res) {
         res.sendStatus(400);
     }
 });
-/*
+
 app.post('/webhook', function(req, res) {
 
     var body = req.body;
 
     // Checks this is an event from a page subscription
-    if (body.object == 'page') {
+    var mode = req.body['hub.mode'];
+    var token = req.body['hub.verify_token'];
+    var challenge = req.body['hub.challenge'];
 
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
 
-            // Gets the message. entry.messaging is an array, but
-            // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
-        });
+        // Checks the mode and token sent is correct
+        if (mode == 'subscribe' && token == VERIFY_TOKEN) {
 
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
     }
 
 });
-*/
+
 // Adds support for GET requests to our webhook
 app.get('/webhook', function(req, res) {
 
