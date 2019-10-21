1496cf68583a76616e67345e603c44be781a6e9b
jpesg <42676743+jpesg@users.noreply.github.com>
2019-10-21 14:54:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -44,20 +44,9 @@ app.get('/webhook', function(req, res){
 app.post('/webhook', function(req, res){
 
     let body = req.body;
-return res.json(req.body)
-    // Checks this is an event from a page subscription
-    if (body.object === 'page') {
 
-        // Iterates over each entry - there may be multiple if batched
-        
 
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-    }
-
+  res.status(200).send('EVENT_RECEIVED');
 });
 
 app.listen(5000, () => {
