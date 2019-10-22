11d0d7892963169d6e81113b741df02abb799f32
jpesg <42676743+jpesg@users.noreply.github.com>
2019-10-22 13:38:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,8 +52,9 @@ app.post('/', function(req, res){
 
             // Gets the message. entry.messaging is an array, but
             // will only ever contain one message, so we get index 0
-            let webhook_event = entry.messaging[0];
-            console.log(webhook_event);
+            //let webhook_event = entry.messaging[0];
+            //console.log(webhook_event);
+            console.log(entry);
         });
 
         // Returns a '200 OK' response to all requests
